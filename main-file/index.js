bc87e1eb8fe7ff1d5ab9e2e0cf2e1bb05e0651d0
Solyman Hosain <120500597+solyman-sh@users.noreply.github.com>
2024-02-05 23:03:02 +0600

    Update index.js

--- main-file/index.js
+++ main-file/index.js
@@ -1,39 +1,4 @@
-// var express = require('express');
-// var multer = require('multer');
 
-// var app = express();
-
-// var storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         cb(null, './uploads/');
-//     },
-//     filename: function (req, file, cb) {
-//         cb(null, file.originalname);
-//     }
-// })
-
-
-// var upload = multer({ storage: storage }).single('file')
-
-
-// app.post('/', function (req, res) {
-
-//     upload(req, res, function (err) {
-//         if (err) {
-//             return res.end('Error uploading file');
-//         }
-//         res.end('File uploaded');
-//     });
-// } );
-
-
-
-// app.listen(3000, function () {
-//     console.log('Example app listening on port 3000!');
-// });
-
-
-// app.js
 
 const http = require('http');
 const fs = require('fs');
